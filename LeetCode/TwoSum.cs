efa505f02c631df2c9a9b32b095024b5f3465c8e
Daniel Li <neolee11@yahoo.com>
2016-02-03 14:06:47 -0500

    Added new LeetCode Project

---
+++ LeetCode/TwoSum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class TwoSum
+    {
+        public int[] GetSolution(int[] nums, int target)
+        {
+            return new int[2];
+        }
+    }
+}
