661970213ce28d8d565c1430ac4192d8559e498f
Daniel Li <neolee11@yahoo.com>
2016-06-21 01:03:01 -0400

    progress

---
+++ LeetCode/_104_Max_Depth_Of_Binary_Tree.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _104_Max_Depth_Of_Binary_Tree
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null) return 0;
+
+            var leftDepth = 1 + MaxDepth(root.left);
+            var rightDepth = 1 + MaxDepth(root.right);
+
+            return Math.Max(leftDepth, rightDepth);
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+}
